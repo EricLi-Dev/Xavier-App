eb6ef932b0a5b87527cddbf515b0d819f8f45431
Eric Li <johnlee888123456@gmail.com>
2019-08-28 16:53:25 -0400

    Tabbed Application Project

---
+++ Xavier HS/Xavier HS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Xavier HS
+//
+//  Created by Eric Li on 8/28/19.
+//  Copyright © 2019 Cornette. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
